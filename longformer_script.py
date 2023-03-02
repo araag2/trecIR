94da2ded290ba8a7ac74ce984c4577038fa36ef5
araag2 <artur.guimas@gmail.com>
2023-03-02 18:23:05 +0000

    added a script to transform a model into a longformer version

---
+++ longformer_script.py
@@ -0,0 +1,68 @@
+import torch
+import argparse
+import os
+
+from tqdm import tqdm
+from torch.optim import AdamW
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2ForSequenceClassification
+
+def create_path(path : set):
+    os.makedirs(path, exist_ok=True)
+    assert os.path.isdir(path)
+
+def create_longformer_tokenizer(model_str : str , max_length : int, save_path : str):
+  tokenizer = GPT2Tokenizer.from_pretrained(model_str, model_max_length = max_length)
+  tokenizer.model_max_length = max_length
+  tokenizer.init_kwargs['model_max_length'] = max_length
+
+  create_path(save_path)
+  tokenizer.save_pretrained(save_path)
+
+def create_longformer_model(model_str : str , max_length : int, save_path : str):
+    model = GPT2ForSequenceClassification.from_pretrained(model_str)
+
+    current_max_pos, embed_size = model.base_model.wpe.weight.shape
+
+    model.config.max_position_embeddings = max_length
+    model.base_model.wpe.num_embeddings = max_length
+    model.base_model.config.n_ctx = max_length 
+
+    new_pos_embed = model.base_model.wpe.weight.new_empty(max_length, embed_size)
+
+    k1 = 0
+    k2 = 0
+    weight = 0.05
+    direction = 0
+
+    while k1 < max_length:
+    if direction == 0: 
+        new_pos_embed[k1] = model.base_model.wpe.weight[k2]
+        k2 += 1
+    else:
+        new_pos_embed[k1] = model.base_model.wpe.weight[k2] + ( weight * model.base_model.wpe.weight[k2-1] )
+        k2 += direction
+    k1 += 1
+    if k2 == 32 and direction != 0: 
+        weight *= 2
+        direction = 1
+    elif k2 == current_max_pos:
+        k2 = current_max_pos - 1
+        weight *= 2
+        direction = -1
+
+    model.base_model.wpe.weight.data = new_pos_embed
+    for layer in tqdm(model.base_model.h):
+        layer.attn.bias = torch.tril(torch.ones((max_length, max_length)), diagonal=1)[None, None, :]
+
+    create_path(save_path)
+    model.save_pretrained(save_path)
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--tokenizer_save_dir', type=str, default="models/BioMedLM-3072/tokenizer/", help='path to model save dir')
+  parser.add_argument('--model_save_dir', type=str, default="models/BioMedLM-3072/SeqClass/", help='path to model save dir')
+  parser.add_argument('--max_length', type=int, default=3072, help='inteded max sequence size')
+  args = parser.parse_args()
+
+  create_longformer_tokenizer("stanford-crfm/BioMedLM", args.max_length, args.tokenizer_save_dir)
+  create_longformer_model("stanford-crfm/BioMedLM", args.max_length, args.model_save_dir)
---
+++ train_GPT.py
@@ -0,0 +1,76 @@
+import torch
+import os
+import json
+import argparse
+from tqdm import tqdm
+from torch.optim import AdamW
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2ForSequenceClassification
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--tokenizer_load_dir', type=str, default="models/pubmedgpt-3072/tokenizer/", help='path to model save dir')
+parser.add_argument('--model_load_dir', type=str, default="models/pubmedgpt-3072/LMHead-fine-tuned/", help='path to model save dir')
+parser.add_argument('--model_save_dir', type=str, default="models/pubmedgpt-3072/LMHead-fine-tuned/", help='path to model save dir')
+parser.add_argument("--CT_input", default="data_json/SemEval2023/", type=str)
+parser.add_argument("--queries_input", default="queries/SemEval2023/", type=str)
+parser.add_argument("--qrels_input", default="qrels/SemEval2023/", type=str)
+args = parser.parse_args()
+
+# Working params: python train_pubmedgpt.py
+
+tokenizer = GPT2Tokenizer.from_pretrained(args.tokenizer_load_dir, max_length=3072)
+model = GPT2LMHeadModel.from_pretrained(args.model_load_dir, max_length=3072)
+
+for name, param in model.named_parameters():
+  if name.split(".")[-1] != "bias" and name != "transformer.ln_f.weight":
+    print(name) 
+    param.requires_grad = False
+
+optimizer = AdamW(model.parameters(), lr=1e-6)
+
+corpus = None
+with open(f'{os.getcwd()}/{args.CT_input}CT_corpus.json') as JSON_Corpus:
+  corpus = json.load(JSON_Corpus)
+
+i = 0
+for ct in tqdm(list(corpus)[500:]):
+  print(ct)
+  for cat in tqdm(corpus[ct]):
+    inputs = tokenizer("\n".join(line for line in corpus[ct][cat]), truncation= True, return_tensors="pt")
+    outputs = model(**inputs, labels=inputs["input_ids"])
+    logits = outputs.logits
+    loss = outputs.loss
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+  i += 1
+  if i % 100 == 0:
+    model.save_pretrained(args.model_save_dir)
+
+model.save_pretrained(args.model_save_dir)
+
+#
+# Train model for document classification, keeping some of the layers fixed
+#
+
+model = GPT2ForSequenceClassification.from_pretrained(args.model_save_dir, num_labels=2)
+quit()
+
+#names = [ "transformer.h." + repr(i) + "." for i in [1,3,5,7,9]] 
+#
+#for name, param in model.named_parameters():
+#    for n in names:
+#      if n in name: param.requires_grad = False
+#optimizer = AdamW(model.parameters(), lr=1e-6)
+#
+#for example in range(10):
+#  inputs = tokenizer("This is example " + repr(example) + " text for training the GPT model.", return_tensors="pt")
+#  labels = torch.nn.functional.one_hot(torch.tensor([10]), num_classes=2).to(torch.float)
+#  outputs = model(**inputs, labels=labels)
+#  logits = outputs.logits
+#  loss = outputs.loss
+#  loss.backward()
+#  optimizer.step()
+#  optimizer.zero_grad()
+#
+#model.save_pretrained(args.model_save_dir)
