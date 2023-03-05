1c119cab53da3d5097048f01ec87ef68fa228e0d
araag2 <artur.guimas@gmail.com>
2023-03-05 17:02:14 +0000

    Implemented LoRA

--- train_gpt.py
+++ train_gpt.py
@@ -14,6 +14,32 @@ def create_path(path : str):
     os.makedirs(path, exist_ok=True)
     assert os.path.isdir(path), f'No such dir: {path}'
 
+def bias_only_grad(model):
+  for name, param in model.named_parameters():
+    if name.split(".")[-1] != "bias":
+      print(name) 
+      param.requires_grad = False
+  return model
+
+def transform_into_LoRA(model, r : int, alpha : float, dropout : float):
+  out_features = 2560 #adapt according to model
+  for module in model.transformer.h:
+    if module.attn != None:
+      lora_c_attn = lora.MergedLinear(
+        out_features, out_features * 3, 
+        r=r, 
+        lora_alpha=alpha, 
+        lora_dropout=dropout, 
+        enable_lora=[True, False, True], 
+        fan_in_fan_out=True,
+        merge_weights=False)
+      lora_c_attn.weight.data = module.attn.c_attn.weight.data
+      lora_c_attn.bias.data = module.attn.c_attn.bias.data
+      module.attn.c_attn = lora_c_attn
+      
+  lora.mark_only_lora_as_trainable(model, bias='lora_only')
+  return model 
+
 def save_lora_state(model, path : str):
   create_path(path)
 
@@ -28,16 +54,20 @@ def load_lora_state(model, ckpt_n_path : str, ckpt_lora_path : str):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('--model_name', type=str, default="BioMedLM-3072", help='model name')
-  parser.add_argument('--exp_name', type=str, default="base", help='describes the conducted experiment')
+  parser.add_argument('--exp_name', type=str, default="MLM-BiasOnly-1kDocs", help='describes the conducted experiment')
   parser.add_argument('--run', type=int, default=0, help='run number for wandb logging')
   parser.add_argument('--load_dir', type=str, default="LMHead/", help='path to model load dir')
-  parser.add_argument('--save_dir', type=str, default="LMHead-fine-tuned/", help='path to model save dir')
+  parser.add_argument('--save_dir', type=str, default="LMHead-MLM-BiasOnly/", help='path to model save dir')
   parser.add_argument("--CT_input", default="datasets/TREC2021/TREC2021_CT_corpus.json", type=str, help='path to JSON for MLM')
   parser.add_argument("--queries_input", default="queries/SemEval2023/", type=str)
   parser.add_argument("--qrels_input", default="qrels/SemEval2023/", type=str)
 
   #Model Hyperparamenters
   parser.add_argument("--lr", type=float, default=1e-6)
+  parser.add_argument("--lora_r", type=int, default=16)
+  parser.add_argument("--lora_dropout", type=float, default=0.1)
+  parser.add_argument("--lora_alpha", type=float, default=0.1)
+
   args = parser.parse_args()
 
   # Working params: python train_pubmedgpt.py
@@ -49,11 +79,9 @@ if __name__ == "__main__":
 
   tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_load_dir, max_length=3072)
   model = GPT2LMHeadModel.from_pretrained(model_load_dir, max_length=3072)
+  model = transform_into_LoRA(model, args.lora_r, args.lora_alpha, args.lora_dropout)
 
-  for name, param in model.named_parameters():
-    if name.split(".")[-1] != "bias" and name != "transformer.ln_f.weight":
-      print(name) 
-      param.requires_grad = False
+  #model = bias_only_grad(model)
 
   corpus = None
   with open(args.CT_input) as JSON_Corpus:
@@ -63,15 +91,15 @@ if __name__ == "__main__":
 
   wandb_config = {'optimizer' : 'AdamW', 'lr' : args.lr}
 
-  wandb.init(
-    project="TREC_LLM-Training",
-    name = f'{args.model_name}/{args.exp_name}/run-{args.run}',
-    group = f'{args.model_name}/{args.exp_name}',
-    config = wandb_config
-  )
+#  wandb.init(
+#    project="TREC_LLM-Training",
+#    name = f'{args.model_name}/{args.exp_name}/run-{args.run}',
+#    group = f'{args.model_name}/{args.exp_name}',
+#    config = wandb_config
+#  )
 
   i = 0
-  for ct in tqdm(list(corpus)):
+  for ct in tqdm(list(corpus)[:1000]):
     inputs = tokenizer("\n".join(line for line in corpus[ct]), truncation= True, return_tensors="pt")
     outputs = model(**inputs, labels=inputs["input_ids"])
     logits = outputs.logits
@@ -80,12 +108,12 @@ if __name__ == "__main__":
     optimizer.step()
     optimizer.zero_grad()
 
-    wandb.log({"train/loss": loss})
+#    wandb.log({"train-loss" : loss, "epoch" : i})
 
     i += 1
-    if i % 1000 == 0:
-      create_path(f'model_save_dir/checkpoint-{i}/')
-      torch.save(model.state_dict(), f'model_save_dir/checkpoint-{i}/')
+    if i % 100 == 0:
+      create_path(f'{model_save_dir}/checkpoint/')
+      torch.save(model.state_dict(), f'{model_save_dir}/checkpoint/checkpoint-{i}.pt')
 
   model.save_pretrained(model_save_dir)
 
