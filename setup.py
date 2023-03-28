e1a831400fb8fe8fa95be00c78e32b4b7959ea1f
araag2 <artur.guimas@gmail.com>
2023-03-28 15:53:04 +0100

    changes to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,2 @@
-    from setuptools import setup, find_packages
-
-    setup(name='TREC-IR', version='1.0', packages=find_packages())
+from setuptools import setup, find_packages
+setup(name='TREC-IR', version='1.0', packages=find_packages())
