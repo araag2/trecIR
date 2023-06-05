8e0474636d1904a0e1a8f040341725da7002e800
araag2 <artur.guimas@gmail.com>
2023-06-05 12:25:59 +0100

    changed file structure again

--- ranking/generate_sintetic_queries.py
+++ ranking/generate_sintetic_queries.py
@@ -4,9 +4,13 @@ import os
 import torch
 import sys
 
+sys.path.insert(0, "../")
+
 from utils_IO import safe_open_w
 from typing import List, Dict
 from transformers import T5Tokenizer, AutoTokenizer, T5ForConditionalGeneration, LongT5ForConditionalGeneration
+
+sys.path.insert(0, "../models/")
 
 # when models is finished, create a setup.py and install the module instead of adding to the path!!!
 from t5 import T5QueryGenerator
@@ -51,20 +55,20 @@ def add_nonoverlap_queries(query_dict : Dict, queries_to_add : Dict) -> None:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--queries', type=str, help='path to base queries file', default="queries/queries2022.json")
+    parser.add_argument('--queries', type=str, help='path to base queries file', default="queries/queries2021.json")
     #"medalpaca/medalpaca-7b" 
-    parser.add_argument('--path_baseline_model', type=str, help='path to dir with baseline model', default="yahma/llama-7b-hf") #"decapoda-research/llama-7b-hf"
-    parser.add_argument('--path_baseline_long_model', type=str, help='path to dir with baseline long-model', default="jmvcoelho/t5-base-msmarco-squad-query-generation-longp-v2")
+    parser.add_argument('--path_model', type=str, help='path to dir with baseline model', default="t5-3b") #"decapoda-research/llama-7b-hf"
 
     # Hyperparameters for query generation
+    parser.add_argument('--max_length', type=int, help='max length of input to process', default=512)
     parser.add_argument('--max_new_tokens', type=int, help='max length of generated query', default=64)
     parser.add_argument('--num_beams', type=int, help='number of beams for beam search', default=1)
     parser.add_argument('--top_k', type=int, help='number of top k tokens to sample from', default=10)
 
-    parser.add_argument('--temperature_list', nargs='+', help='list of models temperatures to run', default=[1.0, 2.0, 4.0, 10.0]) #default=[1.0, 2.0, 4.0, 10.0]
+    parser.add_argument('--temperature_list', nargs='+', help='list of models temperatures to run', default=[1.0]) #default=[1.0, 2.0, 4.0, 10.0]
 
     # Output options and directory
-    parser.add_argument('--return_sequences', type=int, help='number of sequences to return', default=10)
+    parser.add_argument('--return_sequences', type=int, help='number of sequences to return', default=40)
     args = parser.parse_args()
 
     replace_prompts = {'Relevant Query:', 'Given a query:', 'Summarize the query:', 'Generate query:', 'Relevant ', 'Query:', 'Document:', ' . '}
@@ -72,43 +76,41 @@ def main():
     queries = json.load(open(args.queries, 'r'))
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    model_params = {'base_model' : args.path_model, 'max_length' : args.max_length, 'name' : args.path_model.split('/')[-1]}
     temperature_list = args.temperature_list
-    model_params = [{'base_model' : args.path_baseline_model, 'max_tokens' : 300}] 
-    names = ['llama-7b-prompt']
 
     gen_config = GenerationConfig(num_beams=args.num_beams, max_new_tokens=args.max_new_tokens, min_new_tokens=5, 
     top_k=args.top_k, do_sample=True, num_return_sequences=args.return_sequences)
  
-    for model_p, name in zip(model_params, names):
-        queries_expanded = {}
+    queries_expanded = {}
 
-        for query_id in queries:
-            queries_expanded[query_id] = {"0": queries[query_id]}
+    for query_id in queries:
+        queries_expanded[query_id] = {"0": queries[query_id]}
 
-        #model = T5QueryGenerator(base_model=model_p['base_model'], max_tokens=model_p['max_tokens'], device=device)
-        model = LLaMAQueryGenerator(base_model=model_p['base_model'], max_tokens=model_p['max_tokens'], device=device)
+    model = T5QueryGenerator(base_model=model_params['base_model'], max_tokens=model_params['max_length'], device=device)
+    #model = LLaMAQueryGenerator(base_model=model_params['base_model'], max_tokens=model_params['max_length'], device=device)
 
-        for temperature in tqdm(temperature_list):
-            gen_config.temperature = temperature
+    for temperature in tqdm(temperature_list):
+        gen_config.temperature = temperature
 
-            queries_temp_expanded = {}
+        queries_temp_expanded = {}
 
-            new_queries = filter_queries(list(queries.values()), replace_prompts)
-            generated_queries = filter_queries(model.inference(new_queries, batchsize = 1, generation_config = gen_config), replace_prompts) 
-            
-            for i, query_id in enumerate(queries):
-                queries_temp_expanded[query_id] = []
-                base_q = queries[query_id]
-                queries_temp_expanded[query_id].append(base_q)
-                for j in range(args.return_sequences):
-                    queries_temp_expanded[query_id].append(generated_queries[i*args.return_sequences + j])
-                    #queries_temp_expanded[query_id].append(f'{base_q} {generated_queries[i*args.return_sequences + j]}')
+        new_queries = filter_queries(list(queries.values()), replace_prompts)
+        generated_queries = filter_queries(model.inference(new_queries, batchsize = 1, generation_config = gen_config), replace_prompts) 
+        
+        for i, query_id in enumerate(queries):
+            queries_temp_expanded[query_id] = []
+            base_q = queries[query_id]
+            queries_temp_expanded[query_id].append(base_q)
+            for j in range(args.return_sequences):
+                queries_temp_expanded[query_id].append(generated_queries[i*args.return_sequences + j])
+                #queries_temp_expanded[query_id].append(f'{base_q} {generated_queries[i*args.return_sequences + j]}')
 
-            add_nonoverlap_queries(queries_expanded, queries_temp_expanded)
+        add_nonoverlap_queries(queries_expanded, queries_temp_expanded)
 
-        output_dir = f'{args.queries[:-5]}_{name}'
-        with safe_open_w(f'{output_dir}.json') as f:
-            json.dump(queries_expanded, f, indent=4)
+    output_dir = f'{args.queries[:-5]}_{model_params["name"]}'
+    with safe_open_w(f'{output_dir}.json') as f:
+        json.dump(queries_expanded, f, indent=4)
 
 if __name__ == '__main__':
     main()
